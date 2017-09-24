661fc6127df02becdaaa8adb096c34b788b82497
GreyGao <rh87116938@163.com>
2017-09-24 23:42:22 +0800

    add topbar and tabs.

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <template>
   <div id="app">
+    <Topbar></Topbar>
     <h1>欢迎来到新浪微博</h1>
-    <p>
-      <router-link to="/home">首页</router-link>
-      <router-link to="/message">消息</router-link>
-      <router-link to="/discovery">发现</router-link>
-      <router-link to="/me">我</router-link>
-
-    </p>
+    <ul class="tabs">
+      <li><router-link to="/home">首页</router-link></li>
+      <li><router-link to="/message">消息</router-link></li>
+      <li><router-link to="/discovery">发现</router-link></li>
+      <li><router-link to="/me">我</router-link></li>
+    </ul>
     <router-view></router-view>
   </div>
 </template>
@@ -19,6 +19,7 @@
   import Me from './pages/Me.vue'
   import Message from './pages/Message.vue'
   import Discovery from './pages/Discovery.vue'
+  import Topbar from './components/Topbar.vue'
 
   Vue.use(VueRouter)
 
@@ -40,34 +41,33 @@ export default {
     return {
       msg:'hi'
     }
-  }
+  },
+  components:{Topbar}
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" rel="stylesheet/scss">
+  @import "static/css/reset.css";
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #333;
+  
+  .tabs{
+    background: #F5F5F5;
+    display:flex;
+    li{
+      width:25%;
+      text-align: center;
+      padding: 6px 0;
+      a{
+        font-size:15px;
+        color: #5d5d5d;
+      }
+    }
+  }
 }
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
 a {
   color: #42b983;
 }
---
+++ src/components/Topbar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="topbar">
+    <div>GreyGao</div>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'topbar'
+  }
+</script>
+<style lang="scss" rel="stylesheet/scss">
+  .topbar{
+    background: #eee;
+    font-size:20px;
+    margin:5px;
+  }
+</style>
